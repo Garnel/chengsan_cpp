91186f917a6f4f74ebb30a0ef74b633a7cde1c9c
Guangming Mao <maogm12@gmail.com>
2016-07-06 23:28:23 +0800

    Initial Commit

---
+++ chengsan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chengsan
+//
+//  Created by Garnel Mao on 7/6/16.
+//  Copyright © 2016 maogm12@gmail.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
